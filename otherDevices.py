9a73c68c120dcd27051d40e102d1606fd107d562
EvanBenn3 <156371404+EvanBenn3@users.noreply.github.com>
2024-01-13 00:38:11 -0800

    Fixed Screen Issues

--- otherDevices.py
+++ otherDevices.py
@@ -10,23 +10,14 @@ class Screen:
         self.pixelSize = 2
         self.canvas = tk.Canvas(self.root, width=256*self.pixelSize, height=256*self.pixelSize, bg="black")
         self.canvas.pack()
+        # self.canvas.create_rectangle(0, 0, 8, 8, fill="red")
 
     # Updates the Screen
     def update(self):
         x = (self.pos & 0xFF)
         y = (self.pos & 0xFF00) >> 8
-        color = ((self.color & 0b11111) * 8, ((self.color & 0b1111100000) >> 5) * 8, ((self.color & 0b111110000000000) >> 10) * 8)
-
-        self.canvas.create_rectangle(x, y, x+self.pixelSize, y+self.pixelSize, fill="#%02x%02x%02x" % color)
-
-    def display(self):
-        self.canvas.mainloop()
-
-    # Control Signals
-    def screen_io(self, device):
-        if device.io_device_signal_a == True & device.io_device_signal_b == False:
-            self.pos = device.reg_output
-        if device.io_device_signal_a == False & device.io_device_signal_b == True:
-            self.color = device.reg_output
-        if device.io_device_signal_a == True & device.io_device_signal_b == True:
-            self.update()
+        color = ((self.color & 0b11111) * 8 - 1, ((self.color & 0b1111100000) >> 5) * 8 - 1, ((self.color & 0b111110000000000) >> 10) * 8 - 1)
+        # print(x)
+        # print(y)
+        # print(color)
+        self.canvas.create_rectangle(x, y, (x+1)*self.pixelSize * 2, (y+1)*self.pixelSize * 2, fill="#%02x%02x%02x" % color)
